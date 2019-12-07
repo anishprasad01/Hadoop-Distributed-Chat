cd8d9cafb20f6418024829fd1f85ec28a5dbce9a
Ardalan Ahanchi <ardalan@ahanchi.net>
2019-12-07 13:00:49 -0800

    Changed target version. Added base Parser file

---
+++ src/main/java/com/steve/hdc/Parser.java
@@ -0,0 +1,13 @@
+package com.steve.hdc;
+
+/**
+ *  A class which parses the HDC messages which are passed between the chat
+ *  clients and the server. It provides serialization, and deserialization
+ *  to/from the JSON format.
+ *
+ *  @author Ardalan Ahanchi
+ *  @version 1.0
+ */
+public class Parser {
+
+}
