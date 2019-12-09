c6a720b3dbcb9f0760644398899c2886d5ca9118
Anish Prasad <33945547+anishprasad01@users.noreply.github.com>
2019-12-08 18:50:11 -0800

    Added client gui code for sending a message

--- src/main/java/com/steve/hdc/ClientGui.java
+++ src/main/java/com/steve/hdc/ClientGui.java
@@ -11,16 +11,48 @@ public class ClientGui {
     private JTextField sendMessageBox;
     private JTextPane recvMessagePane;
     private JPanel panel1;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JTextField recipientField;
 
     public ClientGui(){
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                char[] passwordArray = passwordField.getPassword();
+                String password = passwordArray.toString();
+                String recipient = recipientField.getText();
+                String toTextBox = null;
+
+                String response;
+
+                response = Client.sendMsg(username, password, new Message(username, recipient, sendMessageBox.getText()));
+
+                if(response == null){
+                    toTextBox = username + ": " + sendMessageBox.getText();
+                }
+                else if(response.equals("Invalid Username/Password.")){
+                    toTextBox = "Please Check your Username and Password";
+                }
+                else if(response.equals("Invalid Sender/Recipient.")){
+                    toTextBox = "Unknown Recipient";
+                }
+                else{
+                    toTextBox = "Something went wrong. Please try again later.";
+                }
+
                 Document doc = recvMessagePane.getDocument();
                 try {
-                    doc.insertString(doc.getLength(), sendMessageBox.getText() + "\n",null);
+                    doc.insertString(doc.getLength(),toTextBox + "\n",null);
                 } catch (BadLocationException ble) {
                     ble.printStackTrace();
+                }
+                Document sendDoc = sendMessageBox.getDocument();
+                try {
+                    sendDoc.remove(0,sendDoc.getLength());
+                } catch (BadLocationException ex) {
+                    ex.printStackTrace();
                 }
             }
         });
