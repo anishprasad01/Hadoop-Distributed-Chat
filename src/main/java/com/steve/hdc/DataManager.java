41983bafb9d234a2b0c60e544f1b40cf9a804c57
Anish Prasad <33945547+anishprasad01@users.noreply.github.com>
2019-12-09 20:36:13 -0800

    Implemented readFile in DataManager

--- src/main/java/com/steve/hdc/DataManager.java
+++ src/main/java/com/steve/hdc/DataManager.java
@@ -33,6 +33,7 @@ public class DataManager{
 
         } catch(IOException e){
             e.printStackTrace();
+            return false;
         }
 //TEMP
         return true;
@@ -55,17 +56,33 @@ public class DataManager{
 
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
     //TEMP
         return true;
     }
 
-    public static InputStream readFile(String filename) throws IOException {
-        InputStream in = null;
-        String str1 = "hdfs://localhost:8080";
-        in = new URL(str1).openStream();
-        IOUtils.copyBytes(in, System.out, 1024, false);
-        return in;
+    public static Message readFile(String filename, String local, String hdfs) throws IOException {
+        try{
+            Process p = Runtime.getRuntime().exec("hdfs dfs –copyToLocal " + hdfs + " " + local);
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            // read the output from the command
+            String s = stdInput.readLine();
+            while (s != null) {
+                System.out.println(s);
+            }
+            // read any errors from the attempted command
+            String s1 = stdError.readLine();
+            while (s1 != null) {
+                System.out.println(s1);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Message(local, true);
     }
 
     public static List<String> fileList(String filename){
