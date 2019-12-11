961d9528e8428a84b1052549c9f9034f02c7a1b3
Pui Sze Pansy Ng <pansyng@uw.edu>
2019-12-10 16:44:43 -0800

    updated DataManager to Java API

--- src/main/java/com/steve/hdc/DataManager.java
+++ src/main/java/com/steve/hdc/DataManager.java
@@ -5,13 +5,29 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.Object;
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.io.IOUtils;
+<<<<<<< Updated upstream
+=======
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+>>>>>>> Stashed changes
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DataManager{
+<<<<<<< Updated upstream
     static{ 
         URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
     }
@@ -90,11 +106,47 @@ public class DataManager{
             while(s1 != null){
                 System.out.println(s);
             }
+=======
+    
+    public static boolean createFolder(String foldername, Configuration conf) throws IOException{
+        FileSystem fs = FileSystem.get(conf);
+        Path path = new Path(foldername);
+        boolean isSuccess = fs.mkdirs(path);
+        fs.close();
+        return isSuccess;
+    }
+    public static FSDataOutputStream createFile(String file, Configuration conf) throws IOException{
+        FileSystem fs  = FileSystem.get(conf);
+        Path path = new Path(file);
+        if(!fs.exists(path)){
+            System.err.println("File " + file + "already exists");
+        }
+        FSDataOutputStream out = fs.create(path);
 
-        } catch(IOException e){
-            e.printStackTrace();
+        return out;
+    }
+
+    public static FSDataInputStream readFile(String file, Configuration conf) throws IOException{
+        FileSystem fs  = FileSystem.get(conf);
+        Path path = new Path(file);
+        FSDataInputStream in = fs.open(path);
+        return in;
+    }
+
+
+>>>>>>> Stashed changes
+
+    public static List<Path> fileList(Path directory) throws IOException{
+        List<Path> arr = new ArrayList<String> ();
+        FileSystem fs = FileSystem.get(conf);
+        RemoteIterator<LocatedFileStatus> i = fs.listFiles(path, true);
+        while(i.hasNext()){
+            LocatedFileStatus fileStatus = i.next();
+            Path p = fileStatus.getPath();
+            arr.add(p)
         }
         return arr;
+
 
     }
 }
