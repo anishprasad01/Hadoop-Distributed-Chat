3aca6dcb5d78109803a68936d32c4d217c8ea7c7
Pui Sze Pansy Ng <pansyng@uw.edu>
2019-12-12 18:35:20 -0800

    Update Server.java

--- src/main/java/com/steve/hdc/Server.java
+++ src/main/java/com/steve/hdc/Server.java
@@ -2,6 +2,8 @@ package com.steve.hdc;
 
 public class Server {
     public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        conf.set
         Message m = new Message("1", "2", "3");
         System.out.println(m);
     }
