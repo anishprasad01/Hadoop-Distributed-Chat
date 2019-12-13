e4ece63e81619d8e815e94e196335fff83dfdb52
Pui Sze Pansy Ng <pansyng@uw.edu>
2019-12-12 17:08:10 -0800

    Update DataManager.java

--- src/main/java/com/steve/hdc/DataManager.java
+++ src/main/java/com/steve/hdc/DataManager.java
@@ -21,52 +21,56 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class DataManager{
-    public static final String DEFAULT_FILE_SYSTEM = "";
-    public
-    public static boolean createFolder(String foldername, Configuration conf) throws IOException{
-        FileSystem fs = FileSystem.get(conf);
+    public static final String DEFAULT_FILE_SYSTEM = "fs.defaultFS";
+    public static final String URI_FILE_SYSTEM = "hdsf://master: 8088"
+
+    public static boolean createFolder(String foldername) throws IOException{
+        Configuration conf = new Configuration();
+        conf.set(DEFAULT_FILE_SYSTEM, URI_FILE_SYSTEM);
+        FileSystem fs = FileSystem.get((Configuration)conf);
         Path path = new Path(foldername);
         boolean isSuccess = fs.mkdirs(path);
         fs.close();
         return isSuccess;
     }
-    public static FSDataOutputStream createFile(String file, Configuration conf) throws IOException{
-        FileSystem fs  = FileSystem.get(conf);
-        Path path = new Path(file);
-        if(!fs.exists(path)){
-            System.err.println("File " + file + "already exists");
-        }
-        FSDataOutputStream out = fs.create(path);
+//     public static FSDataOutputStream createFile(String file, Configuration conf) throws IOException{
+//         FileSystem fs  = FileSystem.get(conf);
+//         Path path = new Path(file);
+//         if(!fs.exists(path)){
+//             System.err.println("File " + file + "already exists");
+//         }
+//         FSDataOutputStream out = fs.create(path);
 
-        return out;
-    }
+//         return out;
+//     }
 
-    public static FSDataInputStream readFile(String local, String hdfs, Configuration conf) throws IOException{
-        FileSystem fs  = FileSystem.get(conf);
-        Path path = new Path(file);
-        FSDataInputStream in = fs.copyToLocalFile(false, hdfs, local, true);
-        return in;
-    }
-
-
+//     public static FSDataInputStream readFile(String local, String hdfs, Configuration conf) throws IOException{
+//         FileSystem fs  = FileSystem.get(conf);
+//         Path path = new Path(file);
+//         FSDataInputStream in = fs.copyToLocalFile(false, hdfs, local, true);
+//         return in;
+//     }
 
 
-    public static List<Path> fileList(Path directory) throws IOException{
-        List<Path> arr = new ArrayList<String> ();
-        FileSystem fs = FileSystem.get(conf);
-        RemoteIterator<LocatedFileStatus> i = fs.listFiles(path, true);
-        while(i.hasNext()){
-            LocatedFileStatus fileStatus = i.next();
-            Path p = fileStatus.getPath();
-            arr.add(p)
-        }
-        return arr;
 
 
-    }
-    public static void
-}
+//     public static List<Path> fileList(Path directory) throws IOException{
+//         List<Path> arr = new ArrayList<String> ();
+//         FileSystem fs = FileSystem.get(conf);
+//         RemoteIterator<LocatedFileStatus> i = fs.listFiles(path, true);
+//         while(i.hasNext()){
+//             LocatedFileStatus fileStatus = i.next();
+//             Path p = fileStatus.getPath();
+//             arr.add(p)
+//         }
+//         return arr;
+
+
+//     }
+//     public static void
+// }
 // 
